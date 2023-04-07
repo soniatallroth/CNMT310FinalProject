131ea5cabf11d726fcad4623415a1daccdef5961
cam elfert <bcheese12345678@gmail.com>
2023-04-07 16:41:42 -0500

    Page class implementation, proper form code

--- login.php
+++ login.php
@@ -1,50 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <!--Bootstrap styleguide-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <!-- Navigation (login specific)-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container">
-        <a class="navbar-brand" href="index.html">Stash</a>
-            <div id="navbarResponsive">
-                <ul class="navbar-nav ms-auto">
-                <li class="nav-item active">
-                </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!--Content-->
-    <main>
-        <form>
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-            <input type="email" class="form-control w-50" />
-            <label class="form-label" for="form2Example1">Email address</label>
-            </div>
-            <!-- Password input -->
-            <div class="form-outline mb-4">
-            <input type="password" class="form-control w-50" />
-            <label class="form-label" for="form2Example2">Password</label>
-            </div>
-            <!-- Submit button -->
-            <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-        </form>
-    </main>
-    <!--Footer-->
-    <footer>       
-    </footer> 
-<!--Bootstrap JS-->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+
+require_once("Page.class.php");
+
+$page = new Page("Home Page");
+
+$page->addHeadElement("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ\" crossorigin=\"anonymous\">");
+$page->addHeadElement("<link rel=\"stylesheet\" href=\"css/style.css\">");
+
+print $page->getTopSection();
+
+print "<!-- Navigation-->";
+print "<nav class=\"navbar navbar-expand-lg navbar-light bg-light fixed-top\">";
+print      "<div class=\"container\">";
+print      "<a class=\"navbar-brand\" href=\"index.php\">Home</a>";
+print           "<div id=\"navbarResponsive\">";
+print               "<ul class=\"navbar-nav ms-auto\">";
+print               "<li class=\"nav-item active\">";
+print               "</li>";
+print               "</ul>";
+print           "</div>";
+print      "</div>";
+print "</nav>";
+print "<!--Content-->";
+print "<main>";
+print '     <h1>Login</h1>';
+print '     <form id="form_login" action="action-login.php" method="POST">';
+print '         <div id="form_login_feedback">';
+print '             <label for="username">Username:</label>';
+print '              <input type="text" id="username" name="username">';
+print '              <br>';
+print '              <label for="password">Password:</label>';
+print '              <input type="password" id="password" name="password">';
+print '              <br><br>';
+print '              <input type="submit" id="submit-btn" value="Login">';
+print '              <br>';
+print '         </div>';
+print '     </form>';
+
+$page->addBottomElement("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>");
+$page->addBottomElement("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>");
+$page->addBottomElement("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>");
+
+print $page->getBottomSection();
+
+?>
