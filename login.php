64911e98d696b80ba9b50dd23aeffa026919dd98
Sonia Tallroth <77209443+soniatallroth@users.noreply.github.com>
2023-04-12 10:43:31 -0500

    updated html

--- login.php
+++ login.php
@@ -5,27 +5,24 @@ require_once("Page.class.php");
 
 $page = new Page("Login Portal");
 
-$page->addHeadElement("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ\" crossorigin=\"anonymous\">");
-$page->addHeadElement("<link rel=\"stylesheet\" href=\"css/style.css\">");
-
+$page->addHeadElement('<link rel="stylesheet" href="css/reset.css">')
+$page->addHeadElement('<link rel="stylesheet" href="css/style.css">')
+    
 print $page->getTopSection();
 
-print "<!-- Navigation-->";
-print "<nav class=\"navbar navbar-expand-lg navbar-light bg-light fixed-top\">";
-print      "<div class=\"container\">";
-print      "<a class=\"navbar-brand\" href=\"index.php\">Home</a>";
-print           "<div id=\"navbarResponsive\">";
-print               "<ul class=\"navbar-nav ms-auto\">";
-print               "<li class=\"nav-item active\">";
-print               "</li>";
-print               "</ul>";
-print           "</div>";
-print      "</div>";
-print "</nav>";
-print "<!--Content-->";
-print "<main>";
-print '     <h1 id="login-header">Login</h1>';
-print '     <form id="form_login" action="action-login.php" method="POST">';
+print   '<!-- Navigation (specific to login)-->';
+print   '<header class="login-header">';
+print   '   <h1><a class="login-logo" href="index.html">stash</a></h1>';
+print   '</header>';
+print   '<!--Content-->';
+print   '<main>';
+print   '<div class="login-container">';
+print   '   <div class="login">';
+print   '             <div class="welcome-text">';
+print   '                 <h2>Welcome!</h2>';
+print   '                 <p>Please enter your details.</p>';
+print   '            </div>';
+print   '            <form id="form_login" action="action-login.php" method="POST">';
 
 if(isset($_SESSION['errors']) && is_array($_SESSION['errors']) && count($_SESSION['errors']) > 0) {
     foreach($_SESSION['errors'] as $field => $message) {
@@ -33,22 +30,16 @@ if(isset($_SESSION['errors']) && is_array($_SESSION['errors']) && count($_SESSIO
     }
     $_SESSION['errors'] = array();
 }
-
-print '         <div id="form_login_feedback">';
-print '             <label for="username">Username:</label>';
-print '             <input type="text" id="username" name="username">';
-print '             <br>';
-print '             <label for="password">Password:</label>';
-print '             <input type="password" id="password" name="password">';
-print '             <br><br>';
-print '             <input type="submit" id="submit-btn" value="Login">';
-print '             <br>';
-print '         </div>';
-print '     </form>';
-
-$page->addBottomElement("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>");
-$page->addBottomElement("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>");
-$page->addBottomElement("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>");
+print   '                   <div id="form_login_feedback">';
+print   '                        <label class="text-label" for="username">Your username</label><br>';
+print   '                        <input class="text-input" id="username" name="username" type="text" placeholder="eg. johnsmith">';
+print   '                        <label class="text-label" for="password">Your password</label><br>';
+print   '                        <input class="text-input" id="password" name="password" type="text" placeholder="********">';
+print   '                        <div class="button-holder">';
+print   '                            <input id="submit-btn" type="submit" value="Login">';
+print   '                        </div>';
+print   '                    </div>';
+print   '                </form>';
 
 print $page->getBottomSection();
 
