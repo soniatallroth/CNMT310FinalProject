563a2281fd31b2e9725ae0e0b3006838feca4b43
Cam Elfert <celfe712@uwsp.edu>
2023-04-13 15:30:54 -0500

    remove old bootstrap files, add new styling

--- bookmarks.php
+++ bookmarks.php
@@ -10,8 +10,8 @@ if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == false) {
     die(header("Location:" . LOGINFORM)); 
 }
 
-$page->addHeadElement("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ\" crossorigin=\"anonymous\">");
-$page->addHeadElement("<link rel=\"stylesheet\" href=\"css/style.css\">");
+$page->addHeadElement('<link rel="stylesheet" href="css/reset.css">');
+$page->addHeadElement('<link rel="stylesheet" href="css/style.css">');
 
 print $page->getTopSection();
 print  "<!-- Navigation -->";
@@ -41,10 +41,6 @@ print  "<!--Footer-->";
 print   "<footer>";       
 print   "</footer>";       
 print "<!--Bootstrap JS-->";
-
-$page->addBottomElement("<script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>");
-$page->addBottomElement("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>");
-$page->addBottomElement("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>");
 
 print $page->getBottomSection();
 
