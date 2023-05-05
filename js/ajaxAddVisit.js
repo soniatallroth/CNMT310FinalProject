fe89a5880d1e8645c6234041ccc95aeca901ba15
cam elfert <bcheese12345678@gmail.com>
2023-05-05 13:32:35 -0500

    add middle-click support for addvisit

--- js/ajaxAddVisit.js
+++ js/ajaxAddVisit.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-        $(".li-title").on("click", function(e){
-            //alert("clicked");
-            //e.preventDefault();
-            var bookmarkID = $(this).data("bookmark-id");
-            $.ajax({
-                url: 'action-addvisit.php',
-                type: 'POST',
-                data: {bookmark_id: bookmarkID},
-                success: function(response) {
-                    location.reload();
-                },
-                error: function(error){
-                    alert(error);
-                }
-            });
+        $(".li-title").on("click auxclick", function(e){
+                //alert("clicked");
+                //e.preventDefault();
+                var bookmarkID = $(this).data("bookmark-id");
+                $.ajax({
+                    url: 'action-addvisit.php',
+                    type: 'POST',
+                    data: {bookmark_id: bookmarkID},
+                    success: function(response) {
+                        location.reload();
+                    },
+                    error: function(error){
+                        alert(error);
+                    }
+                });
         });
     });
