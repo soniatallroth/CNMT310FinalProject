aaed1cbeaa5924fb781749d73fb78e801bec438b
stallroth <sonia.tallroth@gmail.com>
2023-05-08 11:55:45 -0500

    HTML updates for validation purposes

--- bookmarks.php
+++ bookmarks.php
@@ -19,7 +19,7 @@ $books = new Bookmarks();
 
 $page->addHeadElement('<link rel="stylesheet" href="css/reset.css">');
 $page->addHeadElement('<link rel="stylesheet" href="css/style.css">');
-$page->addHeadElement('<link rel="icon" type="image/x-icon" href="images/favicon.ico">');
+$page->addHeadElement('<link rel="icon" href="images/favicon.ico">');
 $page->addHeadElement('<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>');
 $page->addHeadElement('<script src="https://kit.fontawesome.com/c288a0b638.js" crossorigin="anonymous"></script>');
 $page->addHeadElement('<script src="js/ajaxAddVisit.js"></script>');
@@ -56,7 +56,7 @@ print   '                   <label class="text-label" for="url">Add the link for
 print   '                   <input class="text-input" id="url" name="url" type="url" placeholder="https://www.google.com">';
 print   '                   <label class="text-label" for="displayname">Name your bookmark:</label>';
 print   '                   <input class="text-input" id="displayname" name="displayname" type="text" placeholder="e.g. Chili recipe">';
-print   '                   <label class="text-label" for="radio-button-container">Do you want your bookmark to appear in the Popular tab?</label><br>';
+print   '                   <p class="text-label">Do you want your bookmark to appear in the Popular tab?</p><br>';
 print   '                   <div class="radio-button-container">';
 print   '                       <input type="radio" id="public" name="sharingBookmarks" checked="checked" value="public">'; // public radio button
 print   '                       <label for="public">Yes</label><br>'; 
